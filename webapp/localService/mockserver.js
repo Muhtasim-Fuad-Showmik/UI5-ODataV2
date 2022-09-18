1ce9fb64f1969501d41de39015fd54bef058bb82
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-18 17:39:51 +0600

    The button on the UI is functional now

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,7 +1,8 @@
 sap.ui.define([
+	"sap/ui/thirdparty/jquery",
 	"sap/ui/core/util/MockServer",
 	"sap/base/Log"
-], function(MockServer, Log) {
+], function(jQuery, MockServer, Log) {
 	"use strict";
 
 	return {
@@ -19,9 +20,42 @@ sap.ui.define([
 
 			// simulate against the metadata and mock data
 			oMockServer.simulate("../localService/metadata.xml", {
-				sMockdataBaseUrl: "../localService/mockdata",
-				bGenerateMissingMockData: true
+				sMockdataBaseUrl : "../localService/mockdata",
+				bGenerateMissingMockData : true
 			});
+
+			// handling mocking a function import call step
+			var aRequests = oMockServer.getRequests();
+			aRequests.push({
+				method: "GET",
+				path: new RegExp("FindUpcomingMeetups(.*)"),
+				response: function(oXhr) {
+					Log.debug("Incoming request for FindUpcomingMeetups");
+					var today = new Date();
+					today.setHours(0); // or today.toUTCString(0) due to timezone differences
+					today.setMinutes(0);
+					today.setSeconds(0);
+					jQuery.ajax({
+						url: "/Meetups?$filter=EventDate ge " + "/Date(" + today.getTime() + ")/",
+						dataType : 'json',
+						async: false,
+						success : function(oData) {
+							oXhr.respondJSON(200, {}, JSON.stringify(oData));
+						}
+					});
+					return true;
+				}
+			});
+			oMockServer.setRequests(aRequests);
+
+			// handling custom URL parameter step
+			var fnCustom = function(oEvent) {
+				var oXhr = oEvent.getParameter("oXhr");
+				if (oXhr && oXhr.url.indexOf("first") > -1) {
+					oEvent.getParameter("oFilteredData").results.splice(3, 100);
+				}
+			};
+			oMockServer.attachAfter("GET", fnCustom, "Meetups");
 
 			// start
 			oMockServer.start();
